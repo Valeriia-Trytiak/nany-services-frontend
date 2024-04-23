cc6836c52b3df4c10422bf55605976b6e8716fc2
Valeriia Trytiak <valeriia.trytiak@gmail.com>
2024-04-23 12:08:56 +0200

    feat: add svg and file static. Update tailwind-config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,34 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       },
+      colors: {
+        primaryAccent: '#103931',
+        secondaryAccent: 'rgba(16, 57, 49, 0.2)',
+        error: '#DA1414',
+        success: '#3CBC81'
+      }
     },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '20px',
+        tablet: '32px',
+        desktop: '128px'
+      }
+    }
   },
-  plugins: [],
-};
-export default config;
+  plugins: []
+}
+export default config
